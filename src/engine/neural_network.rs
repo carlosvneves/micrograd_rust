eef0dd043676528843392401a47a1188cadbb136
= <=>
2024-07-29 11:12:37 -0300

    refactor

---
+++ src/engine/neural_network.rs
@@ -0,0 +1,3 @@
+pub struct NeuralNetwork {
+    // TODO: implement
+}
--- src/engine/value.rs
+++ src/engine/value.rs
@@ -83,3 +83,28 @@ impl Mul for Value {
         }
     }
 }
+#[cfg(test)]
+mod tests{
+
+    use super::*;    
+    #[test]
+    fn test_add() {
+        let a = Value {
+            data: 4.0,
+            label: Some( "a".to_string()),
+            op: None,
+            children: None,
+        };
+        
+        let b = Value {
+            data: 2.0,
+            label: Some("b".to_string()),
+            op: None,
+            children: None,
+        };
+        
+        let mut d = a + b;
+        d.label = Some("d".to_string());
+        
+        println!("{}",d);
+}
